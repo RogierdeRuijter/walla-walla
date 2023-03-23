d0495e904f2f7f3a2caf441e0dc578e8fee62039
RogierdeRuijter <rogierderuijter@hotmail.com>
2023-03-23 22:16:16 +0100

    1 file modified

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Mijn blog';
-export const SITE_DESCRIPTION = 'Welkom op mijn Blog!';
+export const SITE_TITLE = 'My stories';
+export const SITE_DESCRIPTION = 'These are my stories';
