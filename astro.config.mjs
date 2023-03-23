a55b323aab0bb32c571303eb38fc6cddd2bc0def
RogierdeRuijter <rogierderuijter@hotmail.com>
2023-03-23 22:21:45 +0100

    14 files modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import image from '@astrojs/image';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rogierderuijter.github.io',
-  base: 'walla-walla',
+  site: "https://rogierderuijter.github.io",
+  base: "walla-walla",
   integrations: [mdx(), sitemap(), image()],
 });
